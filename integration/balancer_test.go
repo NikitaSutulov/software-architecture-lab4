f7a09fbf3dd8ac99854b67cf8b50d7927fa920d1
Nikita Sutulov <notsid212@gmail.com>
2023-05-29 22:36:56 +0300

    added integration tests for balancer
    Implemented an integration test and an integration benchmark for load balancer. Also fixed some issues with the docker files for the tests to be run properly.

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -19,14 +19,48 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	numRequests := 3
+
+	addresses := []string{
+		fmt.Sprintf("%s/api/v1/some-data", baseAddress),
+		fmt.Sprintf("%s/api/v1/some-data2", baseAddress),
+		fmt.Sprintf("%s/api/v1/some-data", baseAddress),
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	servers := make([]string, numRequests)
+
+	for i := 0; i < numRequests; i++ {
+		resp, err := client.Get(addresses[i])
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if resp != nil {
+			defer resp.Body.Close()
+			server := resp.Header.Get("lb-from")
+			if server == "" {
+				t.Errorf("Missing 'lb-from' header in response for request %d", i)
+			}
+			servers[i] = server
+		} else {
+			t.Errorf("Response is nil for request %d", i)
+		}
+	}
+
+	if servers[0] != servers[2] {
+		t.Errorf("Different servers for the same address: got %s and %s", servers[0], servers[2])
+	}
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		b.Skip("Integration test is not enabled")
+	}
+	for i := 0; i < b.N; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			b.Error(err)
+		}
+		defer resp.Body.Close()
+	}
 }
